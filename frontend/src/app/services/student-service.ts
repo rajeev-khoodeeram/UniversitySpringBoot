a694fbc1e8755f91d781723c7a0aae5e796ed9c2
Rajeev Khoodeeram <rajeev.khoodeeram@gmail.com>
2025-08-27 19:55:51 -0400

    Committing edit and delete functionalities for student only

--- frontend/src/app/services/student-service.ts
+++ frontend/src/app/services/student-service.ts
@@ -22,17 +22,35 @@ export class StudentService {
     return this.http.get<Student[]>(this.apiUrl);
   }
 
+  /**
+   * Adds a new student.
+   * @param student The student to add.
+   * @returns An observable containing the added student.
+   */
   addStudent(student: Student): Observable<Student> {
     return this.http.post<Student>(`${this.apiUrl}/add`, student);
   }
 
+  /**
+   * Deletes a student by ID.
+   * @param studentId The ID of the student to delete.
+   * @returns An observable indicating the result of the delete operation.
+   */
   deleteStudent(studentId: number): Observable<void> {
     return this.http.delete<void>(`${this.apiUrl}/${studentId}`);
   }
 
+
+  /**
+   * Updates a student by ID.
+   * @param studentId The ID of the student to update.
+   * @param student The updated student data.
+   * @returns An observable containing the updated student.
+   */
   updateStudent(studentId: number, student: Student): Observable<Student> {
     return this.http.put<Student>(`${this.apiUrl}/${studentId}`, student);
   }
+
 
   getStudentById(studentId: number): Observable<Student> {
     return this.http.get<Student>(`${this.apiUrl}/${studentId}`);
